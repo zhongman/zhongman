8f96effe437bd5598f18a6e5d76cfc43be7b1052
钟满 <zhongman2015@163.com>
2017-04-20 23:19:36 +0800

    修改了第3道题

--- week2_3.py
+++ week2_3.py
@@ -1,12 +1,12 @@
 import datetime
 import time
-d1=datetime.date(2017,4,14)
+d1=datetime.date.today()
 print(d1)
 message=input("days:")
 a=int(message)
-if a<2 or a>10:
-    pass
-else:
+list=range(2,10)
+while a in list:
     d=datetime.timedelta(a)
     d2=d1+d
     print(d2)
+    break
