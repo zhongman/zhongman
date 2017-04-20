e252d0fab54dfc75acee36a60271ca0c33084a96
钟满 <zhongman2015@163.com>
2017-04-21 00:08:02 +0800

    又改了

--- week2_3.py
+++ week2_3.py
@@ -4,9 +4,8 @@ d1=datetime.date.today()
 print(d1)
 message=input("days:")
 a=int(message)
-list=range(2,10)
-while a in list:
+b=2**10
+if a>0 and a<b:
     d=datetime.timedelta(a)
     d2=d1+d
     print(d2)
-    break
